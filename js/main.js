c33a716418574f1ef4bd656459a2040bdedf248c
Vany1012 <alvarezvillalvavanya@gmail.com>
2025-03-25 12:04:04 -0600

    Subiendo corrección

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-function sumatoria(numero){
-    suma = 0;
-    for (let i=1; i<=numero; i++){
-        suma += i;
-    }
-    return suma;
-};
-console.log(sumatoria(3));
+const template = `<h1>Hola Mundo!<h1>`;
+
+const body = document.getElementById('body');
+
+body.innerHTML = template;
+body.innerText = template;
